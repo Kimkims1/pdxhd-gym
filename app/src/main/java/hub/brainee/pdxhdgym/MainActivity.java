63c1e65b60722c57888adbd562788423e7afd436
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-01 14:21:01 +0300

    ui-design

--- app/src/main/java/hub/brainee/pdxhdgym/MainActivity.java
+++ app/src/main/java/hub/brainee/pdxhdgym/MainActivity.java
@@ -3,12 +3,26 @@ package hub.brainee.pdxhdgym;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button abtUs,allActivities,allPlans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        //TODO: Create listeners over here
+
+    }
+
+    private void initViews() {
+
+        abtUs = findViewById(R.id.btnAbtUs);
+        allActivities = findViewById(R.id.btnActivities);
+        allPlans = findViewById(R.id.btnPlan);
     }
 }
