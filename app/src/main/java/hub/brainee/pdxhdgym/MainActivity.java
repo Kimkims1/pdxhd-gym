266b67d33bfec2855fddbff6dd641fc43c307336
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-01 15:02:56 +0300

    dbase data

--- app/src/main/java/hub/brainee/pdxhdgym/MainActivity.java
+++ app/src/main/java/hub/brainee/pdxhdgym/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
+        Utils.initTraining();
         //TODO: Create listeners over here
 
     }
